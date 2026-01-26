de54718f01e9f26a321795af44eda14fca63dc0b
Josh <39057715+jwtly10@users.noreply.github.com>
2026-01-26 20:17:07 +0000

    fix annoying lint errors

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,11 @@
-import { defineConfig } from "vite";
+import {defineConfig} from "vite";
 import react from "@vitejs/plugin-react";
+// @ts-ignore
 import tailwindcss from "@tailwindcss/vite";
+// @ts-ignore
 import wails from "@wailsio/runtime/plugins/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), wails("./bindings")],
+    plugins: [react(), tailwindcss(), wails("./bindings")],
 });
